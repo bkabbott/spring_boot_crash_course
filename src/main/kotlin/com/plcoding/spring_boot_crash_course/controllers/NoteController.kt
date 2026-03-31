29be9ac0d871a1d36e61aea7e3bd11b94b04e321
Brian Abbott <hi@brianabbott.com>
2026-03-31 07:51:52 -0400

    continued writing controller

--- src/main/kotlin/com/plcoding/spring_boot_crash_course/controllers/NoteController.kt
+++ src/main/kotlin/com/plcoding/spring_boot_crash_course/controllers/NoteController.kt
@@ -4,19 +4,21 @@ import com.plcoding.spring_boot_crash_course.database.model.Note
 import com.plcoding.spring_boot_crash_course.database.repository.NoteRepository
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 
 // https://localhost:8085/notes
+// GET http://localhost:8085/notes?ownerId=123
 
 @RestController
-@RequestMapping("/api/notes}")
+@RequestMapping("/api/notes")
 class NoteController(
     private val repository: NoteRepository
 ) {
-
 
     data class NoteRequest (
         val id: String?,
@@ -35,15 +37,37 @@ class NoteController(
     )
 
     @PostMapping()
-    fun save(body: NoteRequest) {
-        repository.save(
+    fun save(body: NoteRequest): NoteResponse {
+        val note = repository.save(
             Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
                 title = body.title,
                 content = body.content,
                 color = body.color,
                 createdAt = Instant.now(),
                 ownerId = ObjectId(body.ownerId)
             )
+        )
+
+        return note.toResponse()
+    }
+
+    @GetMapping
+    fun findByOwnerId(
+        @RequestParam(required = true) ownerId: String,
+    ): List<NoteResponse> {
+        return repository.findByOwnerId(ObjectId(ownerId)).map {
+            it.toResponse()
+        }
+    }
+
+    private fun Note.toResponse(): NoteController.NoteResponse {
+        return NoteResponse(
+            id = id.toHexString(),
+            title = title,
+            content = content,
+            color = color,
+            createdAt = createdAt
         )
     }
 
